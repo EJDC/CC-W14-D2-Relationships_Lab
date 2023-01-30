2b69fbdfecd6c1de8c0a2a27dfb9f5d6293edad5
Romain Landolfini <romain.landolfini@gmail.com>
2023-01-30 15:53:14 +0000

    :tada: initial commit

---
+++ src/test/java/com/codeclan/example/employeeservice/EmployeeserviceApplicationTests.java
@@ -0,0 +1,25 @@
+package com.codeclan.example.employeeservice;
+
+import com.codeclan.example.employeeservice.models.Employee;
+import com.codeclan.example.employeeservice.repositories.EmployeeRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeserviceApplicationTests {
+
+	@Autowired
+	EmployeeRepository employeeRepository;
+
+	@Test
+	void contextLoads() {
+	}
+
+	@Test
+	public void createEmployee(){
+		Employee ema = new Employee("Ema", 26, 1232, "ema@email.com");
+		employeeRepository.save(ema);
+	}
+
+}
